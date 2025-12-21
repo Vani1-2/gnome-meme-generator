9d211c0fbddb840b54dfed1a79efaf000d09218e
Giovanni <giovannirafanan609@gmail.com>
2025-12-21 16:29:08 +0800

    changeed directory name

--- deb-build.sh
+++ deb-build.sh
@@ -2,6 +2,6 @@
 set -e
 
 distrobox enter ubuntu -- bash -c '
-  cp ~/Projects/practice-gnome-app/build/src/memerist ~/Projects/ubuntubox/memerist_0.1.0-1_amd64/usr/bin/
-  dpkg-deb --build ~/Projects/ubuntubox/memerist_0.1.0-1_amd64
+  cp ~/Projects/practice-gnome-app/build/src/memerist ~/Projects/ubuntubox/memerist-0.1.0-1-amd64/usr/bin/
+  dpkg-deb --build ~/Projects/ubuntubox/memerist-0.1.0-1-amd64
 '
